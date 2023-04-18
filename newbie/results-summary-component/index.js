4a4d968a0d8ad095798148d54eb45834086ab824
Brian Cross <briancross@shaw.ca>
2023-04-18 13:51:31 -0600

    Implemented JS.

--- newbie/results-summary-component/index.js
+++ newbie/results-summary-component/index.js
@@ -3,8 +3,32 @@ const getData = async () => {
   return await res.json();
 };
 
-getData().then(console.log);
+const updateDOM = data => {
+  const icons = document.querySelectorAll(".summary-category-image");
+  const categories = document.querySelectorAll(".summary-category-title");
+  const scores = document.querySelectorAll(".summary-category-score");
 
-const summaryCategories = document.querySelectorAll(".summary-category");
+  icons.forEach((node, i) => {
+    const img = document.createElement("img");
+    img.setAttribute("src", data[i].icon);
+    img.setAttribute("alt", "");
+    node.appendChild(img);
+  });
 
-console.log(summaryCategories);
+  categories.forEach((node, i) => {
+    node.textContent = data[i].category;
+  });
+
+  scores.forEach((node, i) => {
+    const firstSpan = document.createElement("span");
+    const secondSpan = document.createElement("span");
+
+    firstSpan.textContent = data[i].score;
+    secondSpan.textContent = " / 100";
+
+    node.appendChild(firstSpan);
+    node.appendChild(secondSpan);
+  });
+};
+
+getData().then(updateDOM);
