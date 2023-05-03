f58e3ad17ddcdf4737cde93870882001a9827e33
Brian Cross <briancross@shaw.ca>
2023-05-03 14:20:55 -0600

    Customized theme.

--- intermediate/dictionary-web-app/tailwind.config.js
+++ intermediate/dictionary-web-app/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      serif: ["Lora", "serif"],
+      mono: ["Inconsolata", "mono"],
+    },
+    colors: {
+      fuscia: "#A445ED",
+      red: "#FF5252",
+      white: "#FFFFFF",
+      transparent: "transparent",
+      black: {
+        100: "#3A3A3A",
+        200: "#2D2D2D",
+        300: "#1F1F1F",
+        400: "#050505",
+      },
+      grey: {
+        100: "#F4F4F4",
+        200: "#E9E9E9",
+        300: "#757575",
+      },
+    },
   },
-  plugins: [],
 };
