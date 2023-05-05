96359b7a515c4324549422809843fdfa3e065f02
Brian Cross <briancross@shaw.ca>
2023-05-05 13:06:30 -0600

    Store font setting in local storage. Tweak animation. Modify font handling.

--- intermediate/dictionary-web-app/src/index.ts
+++ intermediate/dictionary-web-app/src/index.ts
@@ -6,46 +6,63 @@ const currentFont = document.getElementById("current-font");
 const fontList = document.getElementById("font-list");
 const fontListArrow = document.getElementById("font-list-arrow");
 const fontListOptions = document.querySelectorAll("#font-list li");
-const fontClasses = ["font-sans", "font-serif", "font-mono"];
+const fonts = {
+  "font-sans": "Sans Serif",
+  "font-serif": "Serif",
+  "font-mono": "Mono",
+};
 
 gsap.set(fontList, { autoAlpha: 0 });
 
 const handleFontListDropdown = e => {
   console.log(e.target);
   if (fontListHidden) {
-    gsap.set(fontListOptions, { autoAlpha: 0, xPercent: 75 });
+    gsap.set(fontListOptions, { autoAlpha: 0, yPercent: -100 });
     gsap.to(fontList, { autoAlpha: 1, duration: 0.3 });
     gsap.to(fontListOptions, {
       autoAlpha: 1,
       ease: "power2.in",
-      duration: 0.8,
+      duration: 0.5,
       stagger: { each: 0.1 },
     });
     gsap.to(fontListOptions, {
-      xPercent: 0,
+      yPercent: 0,
       ease: "power2.out",
-      duration: 0.9,
+      duration: 0.7,
       stagger: { each: 0.1 },
     });
     fontListHidden = false;
   } else {
-    gsap.to(fontList, { autoAlpha: 0, duration: 0.5 });
+    gsap.to(fontList, { autoAlpha: 0, duration: 0.3 });
     fontListHidden = true;
   }
 };
 
-const changeFont = e => {
-  const root = document.documentElement;
-  const font = e.target.dataset.fontOption;
-
-  fontClasses.forEach(className => root.classList.remove(className));
-  root.classList.add(font);
-
-  if (currentFont) currentFont.textContent = e.target.textContent;
+const handleFontSelection = e => {
+  const { fontOption } = e.target.dataset;
+  setFont(fontOption);
 };
+
+const setFont = (fontClassName: string) => {
+  const root = document.documentElement;
+  Object.keys(fonts).forEach(className => root.classList.remove(className));
+  root.classList.add(fontClassName);
+  if (currentFont) currentFont.textContent = fonts[fontClassName];
+  localStorage.setItem("font", fontClassName);
+};
+
+const savedFont = localStorage.getItem("font");
+
+if (!savedFont) {
+  setFont("font-sans");
+} else {
+  setFont(savedFont);
+}
 
 [currentFont, fontListArrow, ...fontListOptions].forEach(node => {
   if (node !== null) node.addEventListener("click", handleFontListDropdown);
 });
 
-fontListOptions.forEach(node => node.addEventListener("click", changeFont));
+fontListOptions.forEach(node =>
+  node.addEventListener("click", handleFontSelection)
+);
